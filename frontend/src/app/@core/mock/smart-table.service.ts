a6869ea07941629deabda9c4cb1c73e87bac07a4
Armando <armando@Armandos-MacBook-Pro.local>
2019-11-24 09:43:29 -0700

    latest version

--- frontend/src/app/@core/mock/smart-table.service.ts
+++ frontend/src/app/@core/mock/smart-table.service.ts
@@ -8,43 +8,57 @@ import 'rxjs/add/operator/map';
 import { tap, catchError } from 'rxjs/operators';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
+import { StyleTypes } from '@swimlane/ngx-charts';
 
 
 @Injectable()
 export class SmartTableService extends SmartTableData {
 
-  data = [{
-    title: 'Tuition',
-    amount: '700',
-    category: 'School',
-  }, {
-    title: 'Car',
-    amount: '300',
-    category: 'Loan',
-  },{
-    title: 'Loans',
-    amount: '200',
-    category: 'School',
-  },{
-    title: 'Subscriptions',
-    amount: '50',
-    category: 'Entertainment',
-  },{
-    title: 'Groceries',
-    amount: '150',
-    category: 'Food',
-  },
-  {
-    title: 'Gas',
-    amount: '80',
-    category: 'Transportation',
-  },
-  {
-    title: 'Aprtment',
-    amount: '1000',
-    category: 'Housing',
-  }];
-
+  data = {
+    name: "Daniel Ornelas",
+      id: 1,
+      income: 2700,
+      state: "tx",
+      expenses: [
+        {
+          title: "Tuition",
+          amount: "100",
+          category: "School"
+        },
+        {
+          title: "Car",
+          amount: "300",
+          category: "Loan"
+        },
+        {
+          title: "Loans",
+          amount: "200",
+          category: "School"
+        },
+        {
+          title: "Subscriptions",
+          amount: "50",
+          category: "Entertainment"
+        },
+        {
+          title: "Groceries",
+          amount: "150",
+          category: "Food"
+        },
+        {
+          title: "Gas",
+          amount: "80",
+          category: "Transportation"
+        },
+        {
+          title: "Aprtment",
+          amount: "1000",
+          category: "Housing"
+        }
+      ]
+    };
+  
+  api_key: string = 'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIwMDk4YmVmMC0zOThlNThmNS04MTllLTQ5ZTMtODgwYy1lNTdjZGEyOGY4MjYiLCJpYXQiOjE1NzQ1ODQwNTAsImV4cCI6MTU3NDU4NTg1MCwic3ViIjoic2JNZW13YXVucEJ5cFJMT0VZMiJ9.T1ISMeoEZozby8wDMy3jFbV21UOJS0hmyUgdMMdstmNSYh3ZASFpJxJui7WGbY5YT84LiLvo9j19HPwoyYGvB2nUNMxU4voycRCjP5tbicGWY6YCuoZTMra6fJm1zsNnatMvDnWX2cbDWjPC78WP80nmlrYCqR7rokx4iyX1H-U4GGgMR3E6AGRZEo7MylmQxUYuweuM7UPyE4k6q70IGKhhcBTvCtjK_pJZ-c_LLkoFR3r4vkLnIQittkF_JXYlDBBxfda407_xcvJ8-j23dbJTRNSYwNCRqC86E2t8rS25UeUMDCmPyJrYDKrU4DoQBKaeME8PmoXh_NxAXtNzMA';
 
   constructor(private httpClient: HttpClient){
     super();
@@ -54,15 +68,20 @@ export class SmartTableService extends SmartTableData {
     return this.httpClient.get('http://172.20.44.4:5000/api/v1/users');
   }
 
+  updateData(income) {
+    this.data['income'] = income;
+    return this.httpClient.post('http://172.20.44.4:5000/api/v1/users/1', this.data);
+  }
+
   getAccount(){
     return this.httpClient.get('https://sandbox.api.yodlee.com/ysl/accounts', {
-      headers: {'Api-Version':'1.1','Authorization':'Bearer eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIwMDk4YmVmMC0zOThlNThmNS04MTllLTQ5ZTMtODgwYy1lNTdjZGEyOGY4MjYiLCJpYXQiOjE1NzQ1Nzg2NzYsImV4cCI6MTU3NDU4MDQ3Niwic3ViIjoic2JNZW13YXVucEJ5cFJMT0VZMiJ9.gsO5IB0Gf62fzY6DRIqaxtmMswdF4T_cAXT-CZe1rqVOwbd9rjtK6IEqPBxsEOc4D4Nn5kIjknTR_Wljn3HiDRWr633mjVVt8leY00NVUIi_FAIZpL6IlEq16W2Z6lYS7TBkQRgWHWT5KR9YNUi38kE1MGK1b8mZ4KQ7OAaBCOtFo1DFZP163FqMJx_XlUzm0_mvyMxdRTnw7_Me4vJ2KpHUJwKJpCP1leK0VKMwhLtOmipOfE9yJCOCYvPB0bdEKr_5zjOHij--H0VahaDxklSm4DG5Sg-eW78A2QZwIxpAqPH4nX_jY9F62w7hI49Z2ExNwbPAYDsqQktxKEn-qQ'}
+      headers: {'Api-Version':'1.1','Authorization':'Bearer '+ this.api_key}
    });
   }
 
   getTransactions(){
     return this.httpClient.get('https://sandbox.api.yodlee.com/ysl/transactions', {
-      headers: {'Api-Version':'1.1','Authorization':'Bearer eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIwMDk4YmVmMC0zOThlNThmNS04MTllLTQ5ZTMtODgwYy1lNTdjZGEyOGY4MjYiLCJpYXQiOjE1NzQ1Nzg2NzYsImV4cCI6MTU3NDU4MDQ3Niwic3ViIjoic2JNZW13YXVucEJ5cFJMT0VZMiJ9.gsO5IB0Gf62fzY6DRIqaxtmMswdF4T_cAXT-CZe1rqVOwbd9rjtK6IEqPBxsEOc4D4Nn5kIjknTR_Wljn3HiDRWr633mjVVt8leY00NVUIi_FAIZpL6IlEq16W2Z6lYS7TBkQRgWHWT5KR9YNUi38kE1MGK1b8mZ4KQ7OAaBCOtFo1DFZP163FqMJx_XlUzm0_mvyMxdRTnw7_Me4vJ2KpHUJwKJpCP1leK0VKMwhLtOmipOfE9yJCOCYvPB0bdEKr_5zjOHij--H0VahaDxklSm4DG5Sg-eW78A2QZwIxpAqPH4nX_jY9F62w7hI49Z2ExNwbPAYDsqQktxKEn-qQ'}
+      headers: {'Api-Version':'1.1','Authorization':'Bearer '+ this.api_key}
    });
   }
 
--- frontend/src/app/pages/extra-components/chat/chat.component.ts
+++ frontend/src/app/pages/extra-components/chat/chat.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
-
+import { Component, NgZone } from '@angular/core';
 import { ChatService } from './chat.service';
+import { HttpClient } from '@angular/common/http';
+import {ApiAiClient} from './ts/ApiAiClient';
+import { SmartTableService } from '../../../@core/mock/smart-table.service';
+
+const botAvatar: string = 'https://i.pinimg.com/originals/0c/67/5a/0c675a8e1061478d2b7b21b330093444.gif';
 
 @Component({
   selector: 'ngx-chat',
@@ -11,9 +15,20 @@ import { ChatService } from './chat.service';
 export class ChatComponent {
 
   messages: any[];
+  chatToggle = false;
+  chatDisplay = "none";
+  loading = false;
+  
+  // Random ID to maintain session with server
+  sessionId = Math.random().toString(36).slice(-5);
 
-  constructor(protected chatService: ChatService) {
-    this.messages = this.chatService.loadMessages();
+  constructor(protected chatService: ChatService, private http: HttpClient, private service : SmartTableService) {
+    this.messages = [];//this.chatService.loadMessages();
+  }
+
+  toggleChat(){
+    this.chatToggle = !this.chatToggle;
+    this.chatDisplay = this.chatToggle? "block" : "none";
   }
 
   sendMessage(event: any) {
@@ -36,9 +51,64 @@ export class ChatComponent {
         avatar: 'https://i.gifer.com/no.gif',
       },
     });
-    const botReply = this.chatService.reply(event.message);
-    if (botReply) {
-      setTimeout(() => { this.messages.push(botReply); }, 500);
-    }
+    // const botReply = this.chatService.reply(event.message);
+    // if (botReply) {
+    //   setTimeout(() => { this.messages.push(botReply); }, 500);
+    // }
+    // this.handleUserMessage(event.message) ;
+
+
+    const client = new ApiAiClient({accessToken: '2cee6503a7744d73846911da73782207'})
+    client
+        .textRequest(event.message)
+        .then((response) => {setTimeout(() => { 
+          this.messages.push(this.addBotMessage(response.result.fulfillment.speech)); 
+          if (response.result.fulfillment.speech.includes("Your income has been changed")){
+            console.log("here");
+            let res = response.result.fulfillment.speech.split(' ');
+            console.log(res[res.length-1]);
+            this.service.updateData(res[res.length-1]).subscribe(data => {console.log(data);});
+          }
+        
+
+        }, 500);
+
+        })
+        .catch((error) => {setTimeout(() => { this.messages.push(this.addBotMessage("Please rephrase your question :(")); }, 500);})
+  }
+
+  // handleUserMessage(text) {
+  //   this.loading = true;
+
+  //   // Make the request 
+  //   this.http.post<any>(
+  //     dialogflowURL,
+  //     {
+  //       sessionId: this.sessionId,
+  //       queryInput: {
+  //         text: {
+  //           text,
+  //           languageCode: 'en-US'
+  //         }
+  //       }
+  //     }
+  //   )
+  //   .subscribe(res => {
+  //     const { fulfillmentText } = res;
+  //     this.addBotMessage(fulfillmentText);
+  //     this.loading = false;
+  //   });
+  // }
+
+  addBotMessage(message){
+    return  {
+      text: message,
+      reply: false,
+      date: new Date(),
+      user: {
+        name: 'Bot',
+        avatar: botAvatar,
+      },
+    };
   }
 }
